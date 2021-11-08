a4b57181e8f334c92b4dd621e13bde2ef1be2ddd
Phil Meyerson <18582693+pmeyerson@users.noreply.github.com>
2021-11-08 15:53:17 -0500

    init

--- app/routes/tutorial.routes.js
+++ app/routes/tutorial.routes.js
@@ -1,15 +1,28 @@
 module.exports = app => {
-    const tutorials = require("../controllers/tutorial.controller");
+    const tutorials = require("../controllers/tutorial.controller.js");
 
     var router = require("express").Router();
 
+    // Create a new Tutorial
     router.post("/", tutorials.create);
+
+    // Retrieve all Tutorials
     router.get("/", tutorials.findAll);
-    router.get("/:id", tutorials.findOne);
-    router.put("/:id", tutorials.update);
-    router.delete("/:id", tutorials.delete);
-    router.delete("/", tutorials.deleteAll);
-    // retreive all published
+
+    // Retrieve all published Tutorials
     router.get("/published", tutorials.findAllPublished);
-    app.use('/api/tutorials', router);
-}
+
+    // Retrieve a single Tutorial with id
+    router.get("/:id", tutorials.findOne);
+
+    // Update a Tutorial with id
+    router.put("/:id", tutorials.update);
+
+    // Delete a Tutorial with id
+    router.delete("/:id", tutorials.delete);
+
+    // Create a new Tutorial
+    router.delete("/", tutorials.deleteAll);
+
+    app.use("/api/tutorials", router);
+};
